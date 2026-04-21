eacbcb92ff44d9207604c60f9039fd3ea9d3146c
Luiz Bessa <bessalfs@gmail.com>
2026-04-20 23:31:49 -0300

    feat: wire AuthProvider and ProtectedRoute into app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { Login } from './pages/Login';
 import { ListagemProjetos } from './pages/ListagemProjetos';
 import { DetalheProjeto } from './pages/DetalheProjeto';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ListagemProjetos />} />
-        <Route path="/projetos/:id" element={<DetalheProjeto />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <ListagemProjetos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/projetos/:id"
+            element={
+              <ProtectedRoute>
+                <DetalheProjeto />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
